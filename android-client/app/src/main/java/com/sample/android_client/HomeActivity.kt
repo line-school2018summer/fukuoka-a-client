29785cb094ea9ba6db9c508a5c73c07993aa1bc3
nanae772 <desperado.kazune@gmail.com>
2018-08-19 20:53:33 +0900

    FriendsActivityをHomeActivityに変更

--- android-client/app/src/main/java/com/sample/android_client/HomeActivity.kt
+++ android-client/app/src/main/java/com/sample/android_client/HomeActivity.kt
@@ -3,10 +3,10 @@ package com.sample.android_client
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class FriendsActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_friends)
+        setContentView(R.layout.activity_home)
     }
 }
