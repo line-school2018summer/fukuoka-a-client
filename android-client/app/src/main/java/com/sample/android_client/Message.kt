ef1a663377263e115004fa8d444caaf4356c4080
Tokky_tane <mitokinksa-dos3zero@outlook.jp>
2018-08-25 18:48:38 +0900

    change messageID
    messageのIDの変数名をIDからmessageIDに変更

--- android-client/app/src/main/java/com/sample/android_client/Message.kt
+++ android-client/app/src/main/java/com/sample/android_client/Message.kt
@@ -2,4 +2,4 @@ package com.sample.android_client
 
 import java.time.LocalDateTime
 
-data class Message(val ID: Int, val senderID: Int, val body: String, val sendTime: LocalDateTime)
+data class Message(val messageID: Int, val senderID: Int, val body: String, val sendTime: LocalDateTime)
