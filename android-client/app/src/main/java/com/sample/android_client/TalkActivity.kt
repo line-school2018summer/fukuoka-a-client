9095e3773af5c800881c9598eaec1fc554b02766
nanae772 <desperado.kazune@gmail.com>
2018-08-26 13:49:50 +0900

    [add]メッセージ入力ボックスと送信ボタンを追加
    トーク画面下部にメッセージ入力ボックスと送信ボタンを追加しました

--- android-client/app/src/main/java/com/sample/android_client/TalkActivity.kt
+++ android-client/app/src/main/java/com/sample/android_client/TalkActivity.kt
@@ -3,6 +3,7 @@ package com.sample.android_client
 import android.os.Bundle
 import android.app.Activity
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 
 import kotlinx.android.synthetic.main.activity_talk.*
 import java.time.LocalDateTime
@@ -19,6 +20,12 @@ class TalkActivity : Activity() {
 
         talk_recycler_view.adapter = MessageRecyclerViewAdapter(messages)
         talk_recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        send_button_talk.setOnClickListener {
+            val message = input_message_box_talk.text.toString()
+            Log.d("TalkActivity", "文字列${message}を送信する")
+            // TODO: message送信処理
+        }
     }
 
 }
