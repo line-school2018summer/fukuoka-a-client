c46121bfb08b04948f7419e82cfe35a1a2bf074b
nanae772 <desperado.kazune@gmail.com>
2018-08-29 11:44:09 +0900

    [add]検索ボックス、ボタン、検索したユーザを列挙するためのrecyclerviewを設置

--- android-client/app/src/main/java/com/sample/android_client/AddFriendsActivity.kt
+++ android-client/app/src/main/java/com/sample/android_client/AddFriendsActivity.kt
@@ -2,11 +2,20 @@ package com.sample.android_client
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_add_friends.*
 
 class AddFriendsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_friends)
+
+        supportActionBar?.title = "友だちを検索"
+
+        search_button_add_friends.setOnClickListener {
+            val pattern = search_box_add_friends.text.toString()
+            Log.d("AddFriendsActivity", "文字列${pattern}が含まれるユーザを検索")
+        }
     }
 }
