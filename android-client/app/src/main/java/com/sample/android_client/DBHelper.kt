2af04b220bb930b4fcd07070057b3711b05bcdf7
Tokky_tane <mitokinksa-dos3zero@outlook.jp>
2018-08-29 12:08:38 +0900

    add UserDBHelper
    ユーザーデータをローカルに保存するためのヘルパークラスを実装しました

---
+++ android-client/app/src/main/java/com/sample/android_client/DBHelper.kt
@@ -0,0 +1,47 @@
+package com.sample.android_client
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+class UsersDBHelper(context: Context) : ManagedSQLiteOpenHelper(context, "localData.db", null, 1) {
+    companion object {
+        private var instance: UsersDBHelper? = null
+        @Synchronized
+        fun getInstance(context: Context): UsersDBHelper {
+            if (instance == null) {
+                instance = UsersDBHelper(context.applicationContext)
+            }
+            return instance!!
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable("Users", true,
+                "ID" to INTEGER + PRIMARY_KEY + UNIQUE,
+                "Name" to TEXT,
+                "Email" to TEXT,
+                "IconID" to INTEGER,
+                "IconURL" to TEXT,
+                "isFriend" to INTEGER)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, newVersion: Int, oldVersion: Int) {
+
+    }
+
+    fun saveUserData(db: SQLiteDatabase, userData: List<User>) {
+        db.transaction {
+            userData.forEach {
+                db.insert("Users",
+                        "ID" to it.ID,
+                        "Name" to it.name,
+                        "Email" to it.email,
+                        "IconID" to it.iconID,
+                        "IconURL" to it.iconURL,
+                        "isFriend" to it.isFriend)
+            }
+        }
+    }
+
+}
