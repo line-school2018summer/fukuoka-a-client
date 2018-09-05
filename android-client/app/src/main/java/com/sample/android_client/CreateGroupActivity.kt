1c144a573c69fba35447bdd5b39322c73c094ec4
nanae772 <desperado.kazune@gmail.com>
2018-09-05 17:23:12 +0900

    [add]友だち追加画面のレイアウトをコピペ
    とりあえず流用できそうな部分はコピペしました

--- android-client/app/src/main/java/com/sample/android_client/CreateGroupActivity.kt
+++ android-client/app/src/main/java/com/sample/android_client/CreateGroupActivity.kt
@@ -1,12 +1,44 @@
 package com.sample.android_client
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.util.Log
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.activity_create_group.*
 
 class CreateGroupActivity : AppCompatActivity() {
+    val groupAdapter = GroupAdapter<ViewHolder>().apply {
+        spanCount = 4
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_group)
+
+        supportActionBar?.title = "新しいグループを作る"
+
+        recycler_view_create_group.apply {
+            layoutManager = GridLayoutManager(this@CreateGroupActivity, groupAdapter.spanCount).apply {
+                spanSizeLookup = groupAdapter.spanSizeLookup
+            }
+            adapter = groupAdapter
+            itemAnimator = null
+        }
+
+        search_button_create_group.setOnClickListener {
+            val keyword = search_box_create_group.text.toString()
+            Log.d("CreateGroupActivity", "文字列${keyword}が含まれるユーザを検索")
+        }
+
+        fab_create_group.setOnClickListener {
+            // TODO: 新しくグループを作ってサーバのDBに登録する処理
+            // TODO: ローカルDBに登録する処理
+        }
+
+        delete_button_create_group.setOnClickListener {
+            search_box_create_group.text.clear()
+        }
     }
 }
