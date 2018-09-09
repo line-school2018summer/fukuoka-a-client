5d38f8358693d50f131d8e851e56fa0dcba82eb8
Tokky_tane <mitokinksa-dos3zero@outlook.jp>
2018-09-10 01:10:21 +0900

    [add] messagesTable
    過去のメッセージを保存するためのテーブル

--- android-client/app/src/main/java/com/sample/android_client/DBHelper.kt
+++ android-client/app/src/main/java/com/sample/android_client/DBHelper.kt
@@ -8,6 +8,7 @@ const val DATABASE_NAME = "local_data.db"
 const val DATABASE_VERSION = 1
 const val USERS_TABLE_NAME = "users"
 const val ROOMS_TABLE_NAME = "rooms"
+const val MESSAGES_TABLE_NAME = "messages"
 
 class DBHelper(context: Context) : ManagedSQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
@@ -35,9 +36,17 @@ class DBHelper(context: Context) : ManagedSQLiteOpenHelper(context, DATABASE_NAM
         db.createTable(ROOMS_TABLE_NAME, true,
                 "id" to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
                 "server_id" to INTEGER + UNIQUE + NOT_NULL,
-                "icon_id" to INTEGER  + NOT_NULL,
+                "icon_id" to INTEGER + NOT_NULL,
                 "name" to TEXT + NOT_NULL,
                 "is_group" to INTEGER + NOT_NULL)
+
+        db.createTable(MESSAGES_TABLE_NAME, true,
+                "id" to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                "server_id" to INTEGER + UNIQUE + NOT_NULL,
+                "room_id" to INTEGER + NOT_NULL,
+                "user_id" to INTEGER + NOT_NULL,
+                "body" to TEXT + NOT_NULL,
+                "posted_at" to TEXT)
 
         // デバッグ用データを追加
         // TODO 今後削除する
