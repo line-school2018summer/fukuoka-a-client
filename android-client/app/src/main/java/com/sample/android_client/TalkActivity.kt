07679d7dd8f33ba2b26771e791ccf0ee6c82667a
Tokky_tane <mitokinksa-dos3zero@outlook.jp>
2018-09-12 02:02:04 +0900

    [add] トーク画面にいる間に投稿されたメッセージを格納する機能

--- android-client/app/src/main/java/com/sample/android_client/TalkActivity.kt
+++ android-client/app/src/main/java/com/sample/android_client/TalkActivity.kt
@@ -5,10 +5,10 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import io.reactivex.Completable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_talk.*
-import org.jetbrains.anko.db.parseList
-import org.jetbrains.anko.db.rowParser
-import org.jetbrains.anko.db.select
+import org.jetbrains.anko.db.*
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 
@@ -18,12 +18,14 @@ class TalkActivity : Activity() {
 
     // TODO Activity起動時に代入するように変更する
     private var roomId = 1
+    private val messages = mutableListOf<Message>()
+
+    var newMessagesCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_talk)
 
-        val messages = mutableListOf<Message>()
         talk_recycler_view.adapter = MessageRecyclerViewAdapter(messages)
         talk_recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
@@ -31,7 +33,7 @@ class TalkActivity : Activity() {
             val message = input_message_box_talk.text.toString()
             Log.d("TalkActivity", "文字列${message}を送信する")
             // TODO: message送信処理
-
+            newMessagesCount++
             // 入力ボックスを空にする
             input_message_box_talk.text.clear()
         }
@@ -40,10 +42,34 @@ class TalkActivity : Activity() {
     override fun onResume() {
         super.onResume()
 
+        newMessagesCount = 0
         val pastMessages = loadPastMessages()
         val recyclerViewAdapter = talk_recycler_view.adapter as MessageRecyclerViewAdapter
 
         recyclerViewAdapter.setMessages(pastMessages)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Completable.fromAction { insertNewMessages(messages.takeLast(newMessagesCount)) }
+                .subscribeOn(Schedulers.io())
+                .subscribe()
+    }
+
+    private fun insertNewMessages(newMessages: List<Message>) {
+        database.use {
+            this.transaction {
+                newMessages.forEach {
+                    this.insert(MESSAGES_TABLE_NAME,
+                            "server_id" to it.serverId,
+                            "room_id" to it.roomId,
+                            "user_id" to it.userId,
+                            "body" to it.body,
+                            "posted_at" to it.postedAt.toString())
+
+                }
+            }
+        }
     }
 
     private fun loadPastMessages(): List<Message> {
