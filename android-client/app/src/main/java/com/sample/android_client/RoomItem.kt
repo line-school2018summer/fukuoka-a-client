1facf3108daaab17b04d9c396d2e5ead5c257e98
nanae772 <desperado.kazune@gmail.com>
2018-09-19 14:06:59 +0900

    [add]とりあえずURLを指定してその画像をアイコンに表示できるようにしました

--- android-client/app/src/main/java/com/sample/android_client/RoomItem.kt
+++ android-client/app/src/main/java/com/sample/android_client/RoomItem.kt
@@ -1,5 +1,6 @@
 package com.sample.android_client
 
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.item_friend_friends.*
@@ -17,6 +18,9 @@ data class RoomItem(val roomId: Int,
         }
 
         // TODO : 相手のアイコンまたはグループアイコンを表示する
+        Picasso.get()
+                .load("https://firebasestorage.googleapis.com/v0/b/fukuoka-a-client.appspot.com/o/image%2F44f40371-93f2-4fcc-a698-329c565896ab?alt=media&token=d2971410-fad5-4c31-9f00-ef9f87ee9b61")
+                .into(viewHolder.circular_imageview_item_friend)
     }
 
     override fun getLayout(): Int = R.layout.item_friend_friends
